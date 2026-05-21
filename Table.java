f54958f370613e7ad289ffb55b80a112369caaa5
rounakp400-wq <rounakp400@gmail.com>
2026-05-21 12:04:22 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,9 @@
+public class Table {
+    public static void main(String[] args)
+{
+        int number = 5;
+        for(int i = 1; i <=10; i++){
+            System.out.println(number + " * " + i + " = " + (number*i));
+        }
+    }
+}
